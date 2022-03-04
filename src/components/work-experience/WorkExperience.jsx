6395ef850b316418447c64c032d7778da7d342e6
Marco Furone <marco.job92@gmail.com>
2022-03-04 09:43:39 +0000

    work-experience section refactor

--- src/components/work-experience/WorkExperience.jsx
+++ src/components/work-experience/WorkExperience.jsx
@@ -4,20 +4,21 @@ import './style/WorkExperience.css'
 import Container from 'react-bootstrap/Container'
 
 function WorkExperience (props) {
-  const Li = ({ job }) => {
-    return (
-      <li>
-        <div class="timeline-image">
-          <img class="rounded-circle img-fluid" src={job.img} alt="" />
+  const Li = ({ job }) =>
+    <li>
+      <div class="timeline-image">
+        <img class="rounded-circle img-fluid" src={job.img} alt="icon" />
+      </div>
+      <div class="timeline-panel">
+        <h4>{job.date}</h4>
+        <h6 class=""><b>{job.company}</b> | {job.location}</h6>
+        <div class="description">
+          {job.description.map(desc => 
+            <div class="paragraph">{desc}</div>
+          )}
         </div>
-        <div class="timeline-panel">
-          <h4>{job.date}</h4>
-          <h6 class=""><b>{job.company}</b> | {job.location}</h6>
-          <p class="">{job.description}</p>
-        </div>
-      </li>
-    )
-  }
+      </div>
+    </li>
 
   return (
     <section class="work-experience">
@@ -29,15 +30,6 @@ function WorkExperience (props) {
           {text.timeline.map(job =>
             <Li job={job} />
           )}
-          <li>
-            <div class="timeline-image">
-              <img class="rounded-circle img-fluid" src="graduate-icon.png" alt="graduate" />
-            </div>
-            <div class="timeline-panel">
-              <h4>March 2016</h4>
-              <h6 class=""><b>Gratuated</b></h6>
-            </div>
-          </li>
         </ul>
       </Container>
     </section>
